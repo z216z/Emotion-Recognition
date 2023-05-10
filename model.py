6c277d038ba87dbeef3fac583a75346b1e034842
Valerio Zingarelli <39964860+z216z@users.noreply.github.com>
2023-05-10 10:24:16 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
 from body_features import BodyEncoder
-from context_path import ContextPath
+from context import Context
+from facial_features import FacialFeatures
 
 class Model(nn.Module):
     def __init__(self, num_classes):
@@ -11,8 +12,10 @@ class Model(nn.Module):
         self.body_encoder = BodyEncoder()
 
         # Context path
-        self.context_path = ContextPath()
+        self.context = Context()
         
+        #Facial Features
+        self.facial_features = FacialFeatures()
         # Fusion
         self.fusion = nn.Sequential(
             nn.Conv2d(1024, 512, kernel_size=1),
@@ -28,13 +31,15 @@ class Model(nn.Module):
         body_features = self.body_encoder(x)
         
         # Pass body features and input image through context path
-        context_features = self.context_path(x)
+        context_features = self.context(x)
         
+        # Pass facial features and input image through facial path
+        facial_features = self.facial_feature_encoder(x)
         # Upsample body features to match size of context features
         body_features = nn.functional.interpolate(body_features, size=context_features.size()[2:], mode='bilinear', align_corners=True)
         
         # Concatenate body and context features along the channel dimension
-        fused_features = torch.cat([body_features, context_features], dim=1)
+        fused_features = torch.cat([body_features, context_features,facial_features], dim=1)
         
         # Pass fused features through fusion layer
         fused_features = self.fusion(fused_features)
