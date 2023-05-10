3e6f350834d53d712352e57933738402fcd95e18
Valerio Zingarelli <39964860+z216z@users.noreply.github.com>
2023-05-10 10:20:36 +0200

    Create facial_features.py

---
+++ facial_features.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+
+class FacialFeatures(nn.Module):
+    def __init__(self):
+        super(FacialFeatures, self).__init__()
+        
+        # Define a CNN for facial feature extraction
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        x = self.pool2(x)
+        return x
